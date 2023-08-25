fa6f0dc68e7d7a437f1fad0ca9e28ddab83703d8
Danesh Kumar <kumardanesh012@gmail.com>
2023-08-25 18:11:10 +0500

    Firt commit

---
+++ java.js
@@ -0,0 +1,42 @@
+function Name(){
+    console.log(document.getElementById("Name").value)
+}
+Name();
+function FName(){
+    console.log(document.getElementById("F_Name").value)
+}
+FName();
+function Email_Adress(){
+    console.log(document.getElementById("Email_Adress").value)
+}
+Email_Adress();
+function Password(){
+    console.log(document.getElementById("Password").value)
+}
+Password();
+function CNIC(){
+    console.log(document.getElementById("CNIC").value)
+}
+CNIC();
+function Mobile(){
+    console.log(document.getElementById("Mobile").value)
+}
+Mobile();
+function Select_Course() {
+    console.log(document.getElementById("Select_Course").value)
+}
+Select_Course();
+function Qualification(){
+    console.log(document.getElementById("Qualification").value)
+}
+Qualification();
+function Select_Gender(){
+    console.log(document.getElementById("Select_Gender").value)
+}
+Select_Gender();
+
+        // Button
+function AddDatta(){
+    console.log(document.getElementById("btn").value)
+}
+AddDatta();
